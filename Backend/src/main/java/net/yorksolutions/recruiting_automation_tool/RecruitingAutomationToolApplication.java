c3cb2663e34828b80ace243a1e3c07003e6c5c75
saad <osman>
2022-07-05 12:18:02 -0500

    Project Start

---
+++ Backend/src/main/java/net/yorksolutions/recruiting_automation_tool/RecruitingAutomationToolApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.recruiting_automation_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitingAutomationToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitingAutomationToolApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/net/yorksolutions/recruiting_automation_tool/RecruitingAutomationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.recruiting_automation_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitingAutomationToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
